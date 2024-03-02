02c270738dcbaa9d8d2a7cf1edbbdf54d7c021f2
jon brookes <marshyon@gmail.com>
2024-03-02 17:45:23 +0000

    single admin item working - appointment types

---
+++ app/Filament/Resources/AppointmentTypeResource/Pages/CreateAppointmentType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\AppointmentTypeResource\Pages;
+
+use App\Filament\Resources\AppointmentTypeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAppointmentType extends CreateRecord
+{
+    protected static string $resource = AppointmentTypeResource::class;
+}
---
+++ app/Filament/Resources/AppointmentTypeResource/Pages/ListAppointmentTypes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\AppointmentTypeResource\Pages;
+
+use App\Filament\Resources\AppointmentTypeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAppointmentTypes extends ListRecords
+{
+    protected static string $resource = AppointmentTypeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
