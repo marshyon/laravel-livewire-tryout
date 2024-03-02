926d761145678cbcbf13d16cfc0ec24223043591
jon brookes <marshyon@gmail.com>
2024-03-02 18:50:35 +0000

    fixed but it was intelephense, not the code

--- app/Filament/Resources/ListingResource.php
+++ app/Filament/Resources/ListingResource.php
@@ -3,13 +3,11 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ListingResource\Pages as ListingPages;
-
 use Filament\Tables;
 use App\Models\Listing;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
-use ListingResource\Pages\EditListing;
 
 class ListingResource extends Resource
 {
@@ -54,10 +52,6 @@ class ListingResource extends Resource
     public static function getPages(): array
     {
         return [
-
-
-            // app/Filament/Resources/ListingResource/Pages/ListListings.php
-
             'index' => ListingPages\ListListings::route('/'),
             'create' => ListingPages\CreateListing::route('/create'),
             'edit' => ListingPages\EditListing::route('/{record}/edit'),
