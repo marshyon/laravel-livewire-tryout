7b53be96a9396d2414b5b0b1f5bf4d5edb85ee4a
jon brookes <marshyon@gmail.com>
2024-03-03 18:09:47 +0000

    test rename create button

--- app/Filament/Resources/AppointmentTypeResource/Pages/ListAppointmentTypes.php
+++ app/Filament/Resources/AppointmentTypeResource/Pages/ListAppointmentTypes.php
@@ -13,7 +13,8 @@ class ListAppointmentTypes extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Create NEW Appointment Type'),
         ];
     }
 }
